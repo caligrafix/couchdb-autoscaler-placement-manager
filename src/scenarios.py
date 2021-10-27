65acff5f6b25ed2364804488ddcfa0be7395a5d0
Francisco Alberto <lemufen@gmail.com>
2021-10-27 07:39:01 -0300

    Refactor Scripts in another file to separate scenarios

--- src/scenarios.py
+++ src/scenarios.py
@@ -2,9 +2,8 @@ import io
 import time
 import logging
 from tqdm import tqdm
-from src.couch import *
-from src.k8s import *
-from src.utils import *
+from src.couch.couch import *
+from src.k8s.k8s import *
 
 
 class TqdmToLogger(io.StringIO):
@@ -150,43 +149,3 @@ def scenario_3_resize_pvc(namespace, pods):
     # Delete pod to recreate and use resized PV
     logging.info(f"Deleting pod")
     delete_pods(pods, namespace)
-
-
-def scenario_4_scaling_pvc_on_demand(namespace, pods, VOLUME_THRESHOLD, MOUNT_VOLUME_PATH):
-    """
-    1. Monitoring the size of PV associate to pod with df command
-    2. If size exceeds the defined threshold
-      5.1. Scale PVC
-    """
-    greater_vol_perc_usage = 0
-
-    logging.info(f"Start Loop")
-
-    pods_volumes_info = get_pods_volumes_info(
-        namespace, pods, MOUNT_VOLUME_PATH
-    )
-
-    pods_over_threshold = []
-
-    for pod, size in pods_volumes_info.items():
-        if size >= VOLUME_THRESHOLD:
-            pods_over_threshold.append(pod)
-
-    greater_pod_vol = max(pods_volumes_info, key=pods_volumes_info.get)
-    greater_vol_perc_usage = pods_volumes_info[greater_pod_vol]
-
-    logging.info(f"% Use of all pods: {pods_volumes_info}")
-    logging.info(f"Pods over threshold: {pods_over_threshold}")
-    logging.info(f"% Pod with greater vol: {greater_pod_vol}")
-    logging.info(f"% Use greater vol: {greater_vol_perc_usage}")
-
-    # Check size is upper VOLUME_UMBRAL
-    if pods_over_threshold:
-        logging.info(f"Resizing PVC of pods {pods_over_threshold}")
-        scenario_3_resize_pvc(namespace, pods_over_threshold)
-    else:
-        logging.info(f"No Volumes to Resize")
-        return 0
-
-    logging.info(
-        f"%Use > 50%, Scaling PVC associated to POD {greater_pod_vol}")
