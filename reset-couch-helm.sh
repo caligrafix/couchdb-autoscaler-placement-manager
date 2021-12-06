6d4c6faf6ae0828fb3091d97148d6f9c2b2f59af
Francisco Alberto <lemufen@gmail.com>
2021-12-06 15:23:03 -0300

    Update reset-couch-helm to deploy custom couchdb chart

--- reset-couch-helm.sh
+++ reset-couch-helm.sh
@@ -1,13 +1,20 @@
+# kubectl delete po couchdb-k8s-stress-tests-s-0
+
 helm uninstall couchdb
 kubectl delete pvc --all
 kubectl delete pv --all
-kubectl delete po couchdb-k8s-stress-tests-s-0
-helm install couchdb couchdb/couchdb --values couch-config.yml 
-docker build -t gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests .
-docker push gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests
+# helm install couchdb couchdb/couchdb --values couch-config.yml 
+helm install couchdb charts/couchdb-3.3.4/couchdb/ --values couch-config.yml
 
-sleep 1m
-kubectl create -f k8s-files/pod.yaml
+#recreate vpa
+# kubectl delete vpa couchdb-vpa
+# kubectl create -f k8s-files/vertical-autoscaler.yaml
 
-sleep 30s
-kubectl logs -f couchdb-k8s-stress-tests-s-0
+# docker build -t gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests .
+# docker push gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests
+
+# sleep 1m
+# kubectl create -f k8s-files/pod.yaml
+
+# sleep 30s
+# kubectl logs -f couchdb-k8s-stress-tests-s-0
