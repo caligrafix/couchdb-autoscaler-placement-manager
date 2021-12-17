015fd220b809d7185f695738101855fd542adbd3
Francisco Alberto <lemufen@gmail.com>
2021-12-17 09:01:10 -0300

    Rename rebuild to rebuild-source-code.sh

---
+++ rebuild-source-code.sh
@@ -0,0 +1,2 @@
+docker build -t gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests . --no-cache
+docker push gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests
