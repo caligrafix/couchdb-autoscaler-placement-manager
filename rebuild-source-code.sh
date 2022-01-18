01b109aa2d170b5b739fb2b47fee1b2a0b938366
Francisco Alberto <lemufen@gmail.com>
2022-01-18 09:11:02 -0300

    Delete unnecessary files

--- rebuild-source-code.sh
+++
@@ -1,2 +0,0 @@
-docker build -t gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests . --no-cache
-docker push gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests
